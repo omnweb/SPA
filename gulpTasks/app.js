ba08def080ec028333bf7e6534858303ab7c78a1
netomatiazi <netoomn@gmail.com>
2020-06-25 17:36:30 -0300

    ADD SPA Project

---
+++ gulpTasks/app.js
@@ -0,0 +1,28 @@
+// tasks relacionadas ao código da aplicação.
+
+const gulp = require('gulp')
+
+function appHTML(cb){
+    return cb()
+}
+
+function appCSS(cb){
+    return cb()
+}
+
+function appJS(cb){
+    return cb()
+}
+
+function appIMG(cb){
+    return cb()
+}
+
+//  Exportar funções usando sistema de modulos do node
+
+module.exports = {
+    appHTML,
+    appCSS,
+    appJS,
+    appIMG
+}
---
+++ gulpTasks/servidor.js
@@ -0,0 +1,16 @@
+//Servidor para ajudar a automatizar o fluxo de desenvolvmento
+
+const gulp = require('gulp')
+
+function monitorarArquivos(cd){
+    return cb()
+}
+
+function servidor(cd){
+    return cb()
+}
+
+module.exports = {
+    monitorarArquivos,
+    servidor
+}
