6f7c4e6ce76f0b26507ef3cbec28c4b2f02a535a
netomatiazi <netoomn@gmail.com>
2020-07-02 17:59:02 -0300

    Server Configuration

--- gulpTasks/app.js
+++ gulpTasks/app.js
@@ -1,43 +1,51 @@
 // tasks relacionadas ao código da aplicação.
-
 const gulp = require('gulp')
-const sass = require('gulp-sass')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
 const uglifycss = require('gulp-uglifycss')
 const concat = require('gulp-concat')
 const htmlmin = require('gulp-htmlmin')
 
-function appHTML(cb){
-    return gulp.src("src/**/*.html")
-        .pipe(htmlmin({collapseWhitespace:true}))
+function appHTML() {
+    return gulp.src('src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('build'))
 }
-function appCSS(){
+
+function appCSS() {
     return gulp.src('src/assets/sass/index.scss') // Pegando o caminho do arquivo raiz que possui a referência para outros arquivos
         .pipe(sass().on('error', sass.logError)) // pega todo o saas e transforma em Css, Caso aconteça um erro ele mostra no log
         .pipe(uglifycss({ "uglyComments": true })) // Acrescenta os arquivos de comentários ao arquivo final
         .pipe(concat('app.min.css'))
-        .pipe(gulp.dest('build/assets/css')) 
+        .pipe(gulp.dest('build/assets/css'))
 }
 
-function appJS(){
-    return gulp.src('src/assets/**/*.js')
-        .pipe(babel({ presets: ["env"] }))
+function appJS() {
+    return gulp.src('src/assets/js/**/*.js')
+        .pipe(babel({ presets: ['ENV'] }))
         .pipe(uglify()) // Não precisa usar parâmentros, ele vai pegar o código e minificar
-        //Usando quando se quer obter uma execução disparada por um evento 
-        .on("error", err => console.log(err))  // Tratando evento de erro, é possível concatenar pipes e on
+        //Usando quando quer obter uma execução disparada por um evento 
+        // .on("error", err => console.log(err))  // Tratando evento de erro, é possível concatenar pipes e on
         .pipe(concat('app.min.js')) // Pega todos os arquivos que foram transpilados usando o babel e minificados usando uglify e concatena em outro arquivo, neste caso o codigo.min.js
-        .pipe(gulp.dest("build/assets/js")) 
+        .pipe(gulp.dest('build/assets/js'))
 }
 
-function appIMG(){
+function appIMG() {
     return gulp.src('src/assets/imgs/**/*.*') //Pega tudo dentro da pasta, sem distição por extensão de arquivo
+        .pipe(gulp.dest('build/assets/imgs'))
 }
+
+//Declarando a task e apontado para a função
+
+gulp.task('appHTML', appHTML)
+gulp.task('appCSS', appCSS)
+gulp.task('appJS', appJS)
+gulp.task('appIMG', appIMG)
 
 //  Exportar funções usando sistema de modulos do node
 
-module.exports = { 
+module.exports = {
     appHTML,
     appCSS,
     appJS,
--- gulpTasks/servidor.js
+++ gulpTasks/servidor.js
@@ -1,13 +1,23 @@
 //Servidor para ajudar a automatizar o fluxo de desenvolvmento
 
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch') // Será usado para monitorar arquivos
 
-function monitorarArquivos(cb){
-    return cb()
+function servidor(cb) {
+    return gulp.src('build')//Vai ficar acompanhando a pasta build
+        .pipe(webserver({
+            port: 8080, //Porta que o servido vai usar
+            open: true, // Se eu quero que abra o browser 
+            livereload: true, // Refresh em tempo real
+        }))
 }
 
-function servidor(cb){
-    return cb()
+// Monitorar arquivos vai servir para verificar se houveram alterações no build da aplicação, ele chama uma das tasks
+
+function monitorarArquivos(cb) {
+    watch('src/**/*.html', () => gulp.series('appHTML')()) // Sempre que for alterado algum arquivo html, essa função arrow é executada para chamar a task de html e efetuar a alteração.
+        return cb()
 }
 
 module.exports = {
